9e6f78c539122dfd64d22717cde1a1c0f5d2bc4f
Lawrence <mail@lawben.com>
2018-03-27 23:35:42 +0200

    travis again

--- test/External_GTest.cmake
+++ test/External_GTest.cmake
@@ -5,6 +5,7 @@ include(ExternalProject)
 ExternalProject_Add(
         googletest
         GIT_REPOSITORY https://github.com/google/googletest.git
+        GIT_TAG cafa9e52285d743d1394900fa95662950f712a72
         UPDATE_COMMAND ""
         INSTALL_COMMAND ""
         BUILD_COMMAND make CXX_FLAGS=${GTEST_CXX_FLAGS} LDFLAGS=${GTEST_LINKER_FLAGS}
